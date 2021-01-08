602c96dc907c0028a23dfa44bcd0b467ba345c50
appulegugure <appulegugure@gmail.com>
2021-01-08 03:26:57 +0000

    ini

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def hello
-      render html: "hello,wor;d!"
+      render html: "hello,worad!"
   end
 end
